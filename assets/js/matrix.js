f0aef85c3590ba76c6d4d503b0493127a7432b7e
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-09-13 11:25:41 -0400

    adjusted settings

--- assets/js/matrix.js
+++ assets/js/matrix.js
@@ -1,4 +1,3 @@
-  // var i=0,timer=setInterval(function(){i==str.length?i==0:i++},1000);
 
   var i = 0;
   setInterval(function(){
@@ -7,7 +6,8 @@
     if (i === str.length) {
       i=0;
     }
-  }, 100);
+  }, 70);
+
   var s = document.documentElement.clientWidth;
   var s2 = document.documentElement.clientHeight;
   var width = q.width = s;
@@ -21,12 +21,12 @@
       // text = String.fromCharCode(3e4+Math.random()*33);
       str = ' Hire me...';
       text = String(str.charAt(i));
-      x_pos = index * 22;
+      x_pos = index * 150;
       q.getContext('2d').fillText(text, x_pos, y_pos);
       letters[index] = (y_pos > 758 + Math.random() * 1e4) ? 0 : y_pos + 15;
     });
   };
 
- setInterval(draw, 100);
+ setInterval(draw, 70);
 
 window.onresize = function(){ location.reload(); }
