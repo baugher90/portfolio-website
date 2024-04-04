cd256035576e2ac403807eedfb2318f676f7b2a3
Ethan Baugher <45408433+baugher90@users.noreply.github.com>
2024-04-04 16:12:22 -0400

    website update and overhaul

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     particles: {
       number: { value: 80, density: { enable: true, value_area: 800 } },
-      color: { value: "#370d91" },
+      color: { value: "#f5f5f5" },
       shape: {
         type: "circle",
         stroke: { width: 0, color: "#000000" },
@@ -21,7 +21,7 @@ particlesJS("particles-js", {
       line_linked: {
         enable: true,
         distance: 150,
-        color: "#370d91",
+        color: "#f5f5f5",
         opacity: 0.4,
         width: 1
       },
