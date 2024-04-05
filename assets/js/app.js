fc119820626be85a860afc84f4eea1e40cafff6b
Ethan Baugher <45408433+baugher90@users.noreply.github.com>
2024-04-05 12:08:27 -0400

    color var updates
    changing color var and refactoring for a them file to make color updates easier

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,14 @@ particlesJS("particles-js", {
       color: { value: "#f5f5f5" },
       shape: {
         type: "circle",
-        stroke: { width: 0, color: "#000000" },
+        stroke: { width: .5, color: "#f5f5f5" },
         polygon: { nb_sides: 5 },
         image: { src: "img/github.svg", width: 100, height: 100 }
       },
       opacity: {
         value: 0.5,
         random: false,
-        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+        anim: { enable: false, speed: .5, opacity_min: 0.1, sync: false }
       },
       size: {
         value: 3,
@@ -22,12 +22,12 @@ particlesJS("particles-js", {
         enable: true,
         distance: 150,
         color: "#f5f5f5",
-        opacity: 0.4,
-        width: 1
+        opacity: 0.3,
+        width: 3
       },
       move: {
         enable: true,
-        speed: 6,
+        speed: 4,
         direction: "none",
         random: false,
         straight: false,
